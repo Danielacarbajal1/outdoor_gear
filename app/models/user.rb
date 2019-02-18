6af522cdce5a771f00b051308f71835484261056
Daniella Depaz <danielladcoding@protonmail.com>
2019-02-18 17:44:27 -0500

    Seeds done, working on index view

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,6 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  validates :first_name, :last_name, :phone_number, :address, presence: true, uniqueness: true
+  validates :phone_number, :address, presence: true, uniqueness: true
+  has_many :gears
 end
