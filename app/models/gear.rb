f16d07d12af202bf4c61927aea6548fd5157b4b4
daniela amyot <daniela.amyot@gmail.com>
2019-02-18 16:11:54 -0500

    done with request form and fixed the request model

--- app/models/gear.rb
+++ app/models/gear.rb
@@ -1,7 +1,7 @@
 class Gear < ApplicationRecord
   belongs_to :user
   validates :category, presence: true, inclusion: { in: ['skiing', 'camping', 'hiking', 'climbing'] }
-  validates :name, :price, presence: true
-  validates :size, inclusion: { in: ['S', 'M', 'L', 'XL'] }
+  validates :name, :price, :description, presence: true
+  validates :size, inclusion: { in: ['XS', 'S', 'M', 'L', 'XL'] }
   has_many :requests, dependent: :destroy
 end
