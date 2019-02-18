ffa92388b38cff3279c2131df03632fb6fe19e58
Gracy Soundron <gracesoundron548@gmail.com>
2019-02-18 13:50:30 -0500

    gears and requests controller

--- app/controllers/requests_controller.rb
+++ app/controllers/requests_controller.rb
@@ -2,31 +2,48 @@ class RequestsController < ApplicationController
   before_action :set_request, only: [:show, :edit, :update, :destroy]
 
   def index
+    @request = Request.all
   end
 
   def show
+    @request = Request.find(params[:id])
   end
 
   def new
+    @request = Request.new
   end
 
   def create
+    @request = Request.new(requests_params)
+
+    if @request.save
+      redirect_to requests_path(@request)
+    else
+      render :new
+    end
   end
 
   def edit
+    @request = Request.find(params[:id])
   end
 
   def update
+    @request = Request.find(params[:id])
+    @request.update(params[:request])
   end
 
   def destroy
+    @request = Request.find(params[:id])
+    @request.destroy
   end
 
   private
 
   def requests_params
+    params.require(:request).permit(:id, :gear_id, :user_id)
   end
 
   def set_request
+    @request = Request.find(params[:id])
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,13 +6,7 @@ Rails.application.routes.draw do
   get 'requests/edit'
   get 'requests/update'
   get 'requests/destroy'
-  get 'users/index'
-  get 'users/show'
-  get 'users/new'
-  get 'users/create'
-  get 'users/edit'
-  get 'users/update'
-  get 'users/destroy'
+
   get 'gears/index'
   get 'gears/show'
   get 'gears/new'
@@ -23,11 +17,10 @@ Rails.application.routes.draw do
   get 'bookings/index'
   get 'bookings/create'
   root to: 'pages#home'
-  resources :user
+
   resources :gear
   resources :request
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :user, do
   resources :gear, do
   resources :request
     end
