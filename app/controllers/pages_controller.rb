79c494779cea4983df68776920465c7a7c659b6c
Daniella Depaz <danielladcoding@protonmail.com>
2019-02-19 17:06:09 -0500

    Added profile page and relevant navbar fixes

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,18 @@ class PagesController < ApplicationController
   def home
     @gears = Gear.all
   end
+
+  def index
+    @gears = @current_user.gears
+  end
+
+  def profile
+    @gears = Gear.where(user_id: current_user)
+  end
+
+  private
+
+  def request_params
+    params.require(:gear).permit(:user_id)
+  end
 end
