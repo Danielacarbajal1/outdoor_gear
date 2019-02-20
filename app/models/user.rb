e298b2b721eb614f6a0cb9324aa425d14f4bc9e0
daniela amyot <daniela.amyot@gmail.com>
2019-02-20 15:55:54 -0500

    fixed map agaiiiin

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  #validates :first_name, :last_name, :email_address, :phone_number, :address, presence: true, uniqueness: true
+  validates :address, presence: true
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   has_many :gears
