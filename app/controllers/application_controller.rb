dcdcd8d78f3b5b8c460f4603ef2433953cf36ff9
Daniella Depaz <danielladcoding@protonmail.com>
2019-02-21 14:42:55 -0500

    Added the profile page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   protected
 
   def configure_permitted_parameters
-    attributes = [:first_name, :last_name, :address, :email, :password]
+    attributes = [:first_name, :last_name, :address, :email, :password, :phone_number]
     devise_parameter_sanitizer.permit(:sign_up, keys: attributes)
     devise_parameter_sanitizer.permit(:account_update, keys: attributes)
   end
