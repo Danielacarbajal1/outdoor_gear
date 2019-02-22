b4a28154d828ae9c68ba5c6ef16e25a36f63d413
Daniella Depaz <danielladcoding@protonmail.com>
2019-02-22 10:29:22 -0500

    Fixed navbar photo issue

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  after_create :set_default_avatar
   mount_uploader :photo, PhotoUploader
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
@@ -8,4 +9,14 @@ class User < ApplicationRecord
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
   has_many :gears
+
+  def set_default_avatar
+    @picture = self.photo
+    if @picture.blank?
+      @picture_user = "https://cdn1.iconfinder.com/data/icons/user-pictures/100/unknown-512.png"
+    else
+      @picture_user = @picture.url
+    end
+    return @picture_user
+  end
 end
